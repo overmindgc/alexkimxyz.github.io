2bd65142811aac180ab96a7d647bc34b618ab600
Alexander Kim <alexander.kim@ualberta.ca>
2018-11-25 20:04:07 -0500

    update

---
+++ deploy_to_master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Unstaged changes:"
+git st -s
+read -n1 -p "Commit changes and deploy to master? [y,n]" doit 
+case $doit in  
+  y|Y) 
+pelican content -s publishconf.py
+git add .
+git ci -m "update"
+git push
+ghp-import output -b master
+git push origin master
+;; 
+  n|N) printf "\n Cancelling\n" 
+;; 
+  *) printf "\n Unrecognized input\n" ;; 
+esac
