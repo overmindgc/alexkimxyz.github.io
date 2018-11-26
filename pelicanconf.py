a718617a5c9bd5c83d00f343d3161b0c0fda888d
Alexander Kim <alexander.kim@ualberta.ca>
2018-11-25 19:45:51 -0500

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Alexander Kim'
 SITENAME = "Alex Kim's Blog"
-SITEURL = 'http://alexkimxyz.github.io'
+SITEURL = 'https://alexkimxyz.github.io'
 
 PATH = 'content'
 
