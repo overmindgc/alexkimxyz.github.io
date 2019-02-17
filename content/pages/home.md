53f52d30acf687dfebdc445aa16c739a71a7b7df
Alexander Kim <alexander.kim@ualberta.ca>
2019-02-17 09:10:06 -0500

    update

--- content/pages/home.md
+++
@@ -1,5 +0,0 @@
-Title: data science by day, sleeping by night
-URL: 
-save_as: index.html
-
-The list of all posts can be found here [here]({category}posts)
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Alexander Kim'
+AUTHOR = 'Alex Kim'
 SITENAME = "Blog"
 SITEURL = 'https://alexkimxyz.github.io'
 
